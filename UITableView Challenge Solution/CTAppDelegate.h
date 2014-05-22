cd21aba1d5c9ab902d554eee879457dfd48125b2
Cyrus Tau <cyrus.tau@gmail.com>
2014-05-21 21:03:02 -0700

    Initial Commit

---
+++ UITableView Challenge Solution/CTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTAppDelegate.h
+//  UITableView Challenge Solution
+//
+//  Created by Monika Gorkani on 5/21/14.
+//  Copyright (c) 2014 Cyrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableView Challenge Solution/CTViewController.h
@@ -0,0 +1,13 @@
+//
+//  CTViewController.h
+//  UITableView Challenge Solution
+//
+//  Created by Monika Gorkani on 5/21/14.
+//  Copyright (c) 2014 Cyrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTViewController : UIViewController
+
+@end
